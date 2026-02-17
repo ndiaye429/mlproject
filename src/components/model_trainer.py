ce6a736d5f0c990fd8df8c8e1cdde09d57631a1b
ndiaye <ndiaye>
2026-02-17 19:56:49 +0100

    Removing some packages

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -12,7 +12,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from xgboost import XGBRegressor
+#from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
